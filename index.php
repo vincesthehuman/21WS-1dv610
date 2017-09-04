8c527664cf6faaa44591a1526f1a7a7288f78488
AlexNaga <an222zd@student.lnu.se>
2017-09-04 10:52:08 +0200

    player

--- index.php
+++ index.php
@@ -8,3 +8,31 @@ $world = "World!";
 
 echo "<h2>Hello, " . $world . "</h2>" . $regularDeck->deckUnshuffled();
 
+echo "<!DOCTYPE html>
+<html>
+
+<head>
+    <title>21</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <link href='stylesheet/style.css' rel='stylesheet'>
+    <meta charset='UTF-8'>
+</head>
+
+<body>
+    <main>
+        <div id='gameBoard'>
+            <h1>Game 21</h1>
+
+            <div class='cards'></div>
+            <div class='scores'></div>
+            <button>Hit</button>
+            <button>Stand</button>
+            <div class='resultText'>
+                <p>Result: </p>
+            </div>            
+        </div>
+    </main>
+</body>
+
+</html>";
+
